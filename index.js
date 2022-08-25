094aaa0858ce064fb10d5608d66a5220d77e34df
vidrr2005 <88354713+vidrr2005@users.noreply.github.com>
2022-08-24 22:09:27 -0600

    Updated files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+$(".botonContacto").click(function() {
+  setTimeout(function () {
+      $(".colContacto").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+  }, 1000);
 
+})
+
+$(".botonUbicacion").click(function() {
+  setTimeout(function () {
+      $(".colUbicacion").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+  }, 1000);
+
+})
